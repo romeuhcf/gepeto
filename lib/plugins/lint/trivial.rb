841e104786021cfe8e577b499390510b87117aa6
Romeu Fonseca <romeu.hcf@gmail.com>
2015-08-24 11:07:41 -0300

    Validando permissoes padrao do spec

--- lib/plugins/lint/trivial.rb
+++ lib/plugins/lint/trivial.rb
@@ -6,3 +6,9 @@ on(:puppet_line) do |env, scope, file, line, lineno|
   env.errors.add(:puppet, file, line, lineno, "Não deveria gerenciar usuários - mover para spec") if line =~ /useradd|adduser/
   env.errors.add(:puppet, file, line, lineno, "Não deveria gerenciar grupos - mover para spec") if line =~ /groupadd|addgroup/
 end
+
+on(:repo_line) do |env, scope, file, line, lineno|
+  if file =~ /\.spec\z/ and line =~ /%.*attr/ and line.include?('-')
+    env.errors.add(:repo, file, line, lineno, "Não deveria usar '-' para definir permissoes. Sugestão, 0640 para arquivos e 0750 para dir")
+  end
+end
