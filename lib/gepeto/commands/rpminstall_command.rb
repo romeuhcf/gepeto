5a35a419f118fa4799e2da58731707e6b00ea459
Romeu Fonseca <romeu.hcf@gmail.com>
2015-09-01 10:08:24 -0300

    Fix rpmbuild call

--- lib/gepeto/commands/rpminstall_command.rb
+++ lib/gepeto/commands/rpminstall_command.rb
@@ -1,25 +1,19 @@
 require 'gepeto/repository'
 require 'gepeto/run_commands'
-module RpmInstallCommand
+class RpmInstallCommand
   include RunCommands
   def validate(rpm_path, repo_root_path)
-    fail('Diretório do repo não encotrado') unless File.directory?(repo_root_path)
-    specs =  Dir[File.join(repo_root_path, '*.spec')]
-    if specs.size > 1
-      fail('Mais de um specfile encontrado no repositório')
-    elsif specs.size < 1
-      fail('Specfile NÃO encontrado no repositório')
+    if repo_root_path
+      fail('Diretório do repo não encotrado') unless File.directory?(repo_root_path)
     end
 
     fail("RPM não encontrado") unless File.file?(rpm_path)
   end
 
   def call(rpm_path, repo_root_path)
-    repository = Gepeto::Repository.new(repo_root_path)
-    repo_root_path = File.expand_path(repo_root_path)
-
-    rpm_path = File.expand_path(rpm_path)
-    rpm_dir = File.dirname(rpm_path)
+    repository     = Gepeto::Repository.new(repo_root_path)
+    rpm_path       = File.expand_path(rpm_path)
+    rpm_dir        = File.dirname(rpm_path)
     dockerfile     = File.join(gepeto_root, "config/rpminstall/Dockerfile")
     buildfile      = File.join(gepeto_root, "config/rpminstall/entrypoint.sh")
     yum_cache_dir  = File.join(gepeto_root, "var/cache/yum")
@@ -47,16 +41,17 @@ end
 desc "<RPM> Provision container and install RPM package".green
 command :rpminstall do |c|
   c.action do |global_options,options,args|
-  c.desc "When informed, use its .extra_repo file to set on yum.repos" # TODO trocar por lista de repos como string e não arquivo para facilitar instrumentação por outros programas
-
-  c.flag [:p,:puppet_dir], :type => String
+    c.desc "When informed, use its .extra_repo file to set on yum.repos" # TODO trocar por lista de repos como string e não arquivo para facilitar instrumentação por outros programas
+    c.flag [:p,:puppet_dir], :type => String
     help_now!("Extra arguments found: '#{args.inspect}'".red.on_yellow) if args.size > 1
     help_now!("Requirement arguments not found: '#{args.inspect}'".red.on_yellow) if args.size < 1
+
     params = [args.shift, options[:repo_dir]]
-    cmd = RpmBuildCommand.new
+    cmd = RpmInstallCommand.new
     begin
       cmd.validate(*params)
     rescue
+      puts "@ #{$!.backtrace.first}...".yellow
       help_now!($!.message.red.on_yellow)
     end
     cmd.call(*params)
